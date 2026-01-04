b1d51babe591d1d1c6a9c536d6573422e6e6e3fe
MK-sigh <glycocalyxes@gmail.com>
2026-01-04 15:10:18 +0900

    BlueskyDataProcessorにコンストラクタを作成。

--- src/main/java/model/BlueskyDataProcessor.java
+++ src/main/java/model/BlueskyDataProcessor.java
@@ -1,6 +1,16 @@
 package model;
+import DAO.PostDAO;
+import DAO.UserDAO;
 
 public class BlueskyDataProcessor {
+    private UserDAO userDao;
+    private PostDAO postDao;
+    
+    public BlueskyDataProcessor(UserDAO userDao, PostDAO postDao){
+        userDao = this.userDao;
+        postDao = this.postDao;
+    }
+
     public void processFeed (String jsonText){}
 
 }
