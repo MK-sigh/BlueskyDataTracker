ee4fb944d513a26b7b7fbc091d8cd68c0e7cf86b
MK-sigh <glycocalyxes@gmail.com>
2026-01-04 15:24:01 +0900

    .

---
+++ src/main/java/processor/BlueskyDataProcessor.java
@@ -0,0 +1,16 @@
+package processor;
+import DAO.PostDAO;
+import DAO.UserDAO;
+
+public class BlueskyDataProcessor {
+    private final UserDAO userDao;
+    private final PostDAO postDao;
+    
+    public BlueskyDataProcessor(UserDAO userDao, PostDAO postDao){
+        this.userDao = userDao;
+        this.postDao = postDao;
+    }
+
+    public void processFeed (String jsonText){}
+
+}
