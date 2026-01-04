9b316516ee478ba087860a2c5de9c44772555d24
MK-sigh <glycocalyxes@gmail.com>
2026-01-05 02:47:12 +0900

    static 修正

--- src/main/java/tracker/BlueskyTrackerApplication.java
+++ src/main/java/tracker/BlueskyTrackerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling // スケジューリングを有効化
 public class BlueskyTrackerApplication {
-    public void main (String[] args){
+    public static void main (String[] args){
         SpringApplication.run(BlueskyTrackerApplication.class,args);
     }
     
