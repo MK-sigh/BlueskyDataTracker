28a825b813f4c1e13f988dbf631de3b320713627
MK-sigh <glycocalyxes@gmail.com>
2026-01-07 14:51:54 +0900

    DAOの内容を記述

--- src/main/java/tracker/DAO/PostTagDAO.java
+++ src/main/java/tracker/DAO/PostTagDAO.java
@@ -1,5 +1,11 @@
 package tracker.DAO;
 
-public class PostTagDAO {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tracker.model.PostTag;
+import tracker.model.PostTagPK; // 先ほど作った複合主キー・クラス
+
+@Repository
+// IDの型が Integer ではなく PostTagPK になる点に注意！
+public interface PostTagDAO extends JpaRepository<PostTag, PostTagPK> {
 }
--- src/main/java/tracker/DAO/TagDAO.java
+++ src/main/java/tracker/DAO/TagDAO.java
@@ -1,5 +1,16 @@
 package tracker.DAO;
 
-public class TagDAO {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tracker.model.Tag;
+
+@Repository
+public interface TagDAO extends JpaRepository<Tag, Integer> {
+    // データベース操作（CRUD操作）を簡単に行うためのインターフェース
+    // JpaRepository<扱うエンティティ, 主キーの型> を継承する
+    // 文字列（タグ名）からTagエンティティを探すメソッド
+    // SQL: SELECT * FROM tags WHERE tag = ?
+    Optional<Tag> findByTag(String tag);
 }
