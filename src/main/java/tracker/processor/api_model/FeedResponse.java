c6d5f60c21143bc967de761d9b643441b612f00e
MK-sigh <glycocalyxes@gmail.com>
2026-01-10 17:30:31 +0900

    jsonの記述に合わせ、feedをpostsに変更

--- src/main/java/tracker/processor/api_model/FeedResponse.java
+++ src/main/java/tracker/processor/api_model/FeedResponse.java
@@ -3,14 +3,14 @@ package tracker.processor.api_model;
 import java.util.List;
 
 public class FeedResponse {
-    private List<ItemFeedJson> feed;
+    private List<PostViewJson> posts;
     private String cursor;
 
     // Jacksonが値を設定・取得できるように、ゲッターとセッターが必要です
     public FeedResponse() {}
-    
-    public List<ItemFeedJson> getFeed() { return feed; }
-    public void setFeed(List<ItemFeedJson> feed) { this.feed = feed; }
+
+    public List<PostViewJson> getPosts() { return posts; }
+    public void setPosts(List<PostViewJson> posts) { this.posts = posts; }
     public String getCursor() { return cursor; }
     public void setCursor(String cursor) { this.cursor = cursor; }
 
